af6e454d8c5d268e9ac444a33c18b8c63cda6891
pajowu <github@ca.pajowu.de>
2016-06-10 15:07:02 +0300

    fixed deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null site/* pajowu@pajowu.de:/srv/http/minkorrekt
+scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null site/* pajowu@pajowu.de:/srv/http/minkorrekt
